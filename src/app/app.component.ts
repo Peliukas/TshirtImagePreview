19d8a5bd5dbe7e9cef8c462c4fbd0878ed04b382
Peliukas <pavel.kocia@gmail.com>
2018-07-02 00:11:23 +0200

    Initial commit: - Implemented t-shirt image switcher; - Added overlay image upload and position adjustment function; - Applied minimal styling;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  public images: any[] = [
+    '../assets/images/tshirts/blue.jpg',
+    '../assets/images/tshirts/red.jpg',
+    '../assets/images/tshirts/green.jpg'
+  ];
+
+  public adjustImagePositionInput: number = 100;
+  public uploadedImage: any;
+  public fileName: string;
+  public selectedTshirtImage = '../assets/images/tshirts/blue.jpg';
+
+  onFileChange(event) {
+    console.log(event.files);
+    if (event.target.files[0]) {
+      this.fileName = event.target.files[0].name;
+      const reader = new FileReader();
+      reader.onload = (event: any) => {
+        this.uploadedImage = event.target.result;
+      };
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+
+  public imagePositionChange(event: any) {
+
+    this.adjustImagePositionInput = parseInt(event.srcElement.value) + 100;
+    console.log(this.adjustImagePositionInput);
+  }
 }
